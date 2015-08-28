f74244471fcaef45d1196feecdd7b90bb76acba9
Atzmon Ghilai <atzmon.ghilai@gmail.com>
2015-08-28 22:33:46 +0300

    first version of hello world

---
+++ hello Csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            int result = number1 * number2;
+            Console.WriteLine("the result is:" + result);
+            Console.ReadLine();
+
+        }
+    }
+}
