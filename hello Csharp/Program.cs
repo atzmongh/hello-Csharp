d988b98d8e67656045a8711b36ff3ab9fc2d729f
Atzmon Ghilai <atzmon.ghilai@gmail.com>
2015-08-28 22:43:32 +0300

    second version of hello world app

--- hello Csharp/Program.cs
+++ hello Csharp/Program.cs
@@ -11,9 +11,11 @@ namespace hello_Csharp
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("Please enter 2 numbers to multiply");
             int number1 = int.Parse(Console.ReadLine());
             int number2 = int.Parse(Console.ReadLine());
 
+           
             int result = number1 * number2;
             Console.WriteLine("the result is:" + result);
             Console.ReadLine();
